2d79c24af9b25ffef54907fd1b93b2d10c4ec61b
pbharatprdxn <bharat.poptwani.prdxn@gmail.com>
2016-07-25 07:48:52 +0530

    further explanation added

--- global_env/script.js
+++ global_env/script.js
@@ -4,4 +4,5 @@
 - The Execution context creates two things pre-emptively for us
 	- A relevant environment specific object. [In case of the browser environment, the global object is 'Windows' which is created]
 	- A  special 'this' variable.
+- When you run this code in the browser and check the 'this' variable in the console, you should see it pointing to the 'window' object
 */
