707df8a3833dc98d6457acbf968f340f867a4d80
pbharatprdxn <bharat.poptwani.prdxn@gmail.com>
2016-07-25 07:45:26 +0530

    global execution context example

---
+++ global_env/script.js
@@ -0,0 +1,7 @@
+
+/*
+- Even when no code is written, the javascript engine creates an execution context.
+- The Execution context creates two things pre-emptively for us
+	- A relevant environment specific object. [In case of the browser environment, the global object is 'Windows' which is created]
+	- A  special 'this' variable.
+*/
