a418f0c366c4e1a7ce63c9badbb2b574d8668cd0
Bharat Poptwani <bharat@sixmod5.com>
2016-08-04 12:46:30 +0530

    bracket notation quirkiness explained with an example

---
+++ bracket_notation_quirk/script.js
@@ -0,0 +1,51 @@
+/*
+ We can access object's properties in two ways
+ - dot notation
+ - Bracket notation
+
+ When accessing properties with bracket notation, always ensure that the property names are mentioned as 'strings'.
+ because if they are NOT, it can lead to very undesirable result as we will see below
+*/
+
+var foo = {
+  'unique_prop': 1
+};
+
+var bar = {
+  'unique_prop': 2
+};
+
+var object = {};
+
+object[foo] = "value";
+
+console.log(object[bar]); //This will return "value"
+
+/*
+  Unreal right?
+  Makes no sense!
+
+  Lets walk the code line by line
+
+  Line 10, we are declaring 'foo' as an object with a property 'unique_prop' and it's value 1
+
+  Line 14, we are declaring 'bar' as an object with a property 'unique_prop' and it's value 2
+
+  Line 15, we are declaring and empty object literal
+
+  Line 20, we are assigning 'foo' as a property of 'object' Object, now since we haven't written property name 'foo' as a string, it converts it to a string.
+  This is basically what happens behind the scenes
+  object[foo.toString()] = 'value'
+
+  Now since foo is already declared as an object on line 10, so foo.toString() converts to '[object object]', you can check by doing console.log(foo.toString())
+
+  So Line 20 basically becomes
+  object = {
+    '[object object]' = 'value'
+  }
+
+  Now, when we are trying to access bar property in line no 22, again we aren't accessing it as a string, so again it does console.log(object[bar.toString()]); behind the scenes
+
+  Line 22 basically is console.log(object['[object object]']);
+  And since on line 20 '[object object]' property was set to 'value', line 22 returns 'value'
+*/
