9961c289fee6a819973a56b066950d99a62044ed
pbharatprdxn <bharat.poptwani.prdxn@gmail.com>
2016-08-05 07:12:16 +0530

    operators explained

---
+++ operators/script.js
@@ -0,0 +1,9 @@
+/*
+	Operators are functions which are called syntactically differently than one would normally calls functions. it usually takes in two parameters.
+
+	var result = 3 + 4;
+	Here plus(+) is an operator and it has two paramters 3 and 4.
+	It is called an 'infix' operator because its in between two paramters.
+	+3 4 would've been 'prefix' operator
+	3 4+ would've been 'postfix' operator
+*/
