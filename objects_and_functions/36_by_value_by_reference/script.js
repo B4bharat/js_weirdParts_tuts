dc9bc468c853e39627b2bdd7aa2198e174863192
Bharat Poptwani <b4bharat.available@gmail.com>
2016-08-25 21:31:18 +0530

    'passing by reference' explained

--- objects_and_functions/36_by_value_by_reference/script.js
+++ objects_and_functions/36_by_value_by_reference/script.js
@@ -7,3 +7,16 @@ a = 2; //new value '2' is added to the memory location referenced by variable 'a
 
 console.log(a); //2
 console.log(b); //3
+
+//by reference (all objects including functions)
+var object1 = { 'greeting': 'hi' };
+var object2;
+
+object2 = object1; //unlike 'passing by value', here object2 will point to the same memory location as 'object1', so will have same value as 'object1' but this also ensures that in future when 'object1's value is changed, it will change 'object2's value as well and vice-versa
+console.log(object1); // { 'greeting': 'hi' }
+console.log(object2); // { 'greeting': 'hi' }
+
+object1.greeting = 'hello'; //since both 'object1' and 'object2' are pointing to the same memory location, both will have the same value 'hello' now
+console.log(object1);// { 'greeting': 'hello' }
+console.log(object2); // { 'greeting': 'hello' }
+
