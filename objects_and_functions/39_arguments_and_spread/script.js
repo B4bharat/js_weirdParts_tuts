a625022581adf361de044496e8ace1178d06859e
Bharat Poptwani <b4bharat.available@gmail.com>
2016-08-29 23:26:00 +0530

    added a few function argument scenarios

--- objects_and_functions/39_arguments_and_spread/script.js
+++ objects_and_functions/39_arguments_and_spread/script.js
@@ -38,4 +38,26 @@ clubInfo('Arsenal', 5, 1886); //will log 'Arsenal', 5, 1886
 	The above example shows how every argument is tightly coupled with their corresponding named parameter
 	If you change the value of a named paramter, the value of the corresponding index's argument also changes and vice-versa
 */
+console.log('--------------------------');
 
+(function(a, b, c) {
+	//'arguments' has two members
+	console.log(arguments.length); //2
+
+	//Updating arguments[2] should not modify named parameters
+	arguments[2] = 10;
+	console.log(c); //undefined
+})(1, 2);
+/*
+Argument properties whose index is greater than or equal to the number of formal parameters (i.e. additional arguments which do not correspond to named parameters) are not bound to any named parameter value
+*/
+
+
+/*
+Similarly if a function call does not supply an argument for every named paramter, the unfulfilled parameters should not be bound to the arguments object and their values cannot be updated by modifying the arguments objects
+*/
+
+(function(a, b, c) {
+	c = 10;
+	console.log('2' in arguments); //false
+})(1, 2);
