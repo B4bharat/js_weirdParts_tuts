aa2ee4a50dab856e2a7a0fd0c1e67e4964bb6723
Bharat Poptwani <b4bharat.available@gmail.com>
2016-08-31 23:40:17 +0530

    accessing global execution context from IIFE explained

--- objects_and_functions/45_IIFE_safe_code/greet.js
+++ objects_and_functions/45_IIFE_safe_code/greet.js
@@ -1,5 +1,12 @@
-(function (name) {
+(function (global, name) {
 	var greeting = 'Hello '+ name;
 	/*Since IIFE creates a function scope, this 'greeting' variable is in the function scope, it dooesn't tinkers with the 'greeting' variable in script.js which is in the 'global scope'*/
 	console.log(greeting);
-}('Bharat'));
+
+	global.greeting = 'Hello';
+	/*
+		When you puposefully want to modify a variable in the global execution context or make something available globally(publicly)
+	*/
+}(window, 'Bharat'));
+
+console.log(greeting); //will log the value of the variable 'greeting' which is in the 'global scope' which is 'Hola'
