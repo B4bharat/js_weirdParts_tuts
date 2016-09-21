8b2074016fe90eacbd66886bf1f58af106b42e6d
Bharat Poptwani <b4bharat.available@gmail.com>
2016-09-21 23:34:35 +0530

    example and explanation of a function expression

--- rev_objects_and_functions/35_func_stmt_and_func_expression/script.js
+++ rev_objects_and_functions/35_func_stmt_and_func_expression/script.js
@@ -10,3 +10,17 @@ function greet() {
 The greet property according to Tony Alicia is invocable by paranthesis, so line no. 20 does exactly that, it invokes greet function's code property
 */
 greet();
+
+/*
+- The right side of the 'assignment' operator creates a function object and saves it in the memory and the variable 'anonymousGreet' points to that place in memory.
+- The name property of this function has no value as we can clearly see the function isn't given any name, so its an anonymous function.
+- The code property contains the console.log statement and can be invoked using the variable that is pointing towards it.
+*/
+var anonymousGreet = function() {
+	console.log('hello from inside a function expression baby!');
+};
+
+/*
+	Function invocation statement below
+*/
+anonymousGreet();
