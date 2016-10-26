5667d8bed28fc60896d1fe4ba2a2f1b70a9885d3
Bharat Poptwani <b4bharat.available@gmail.com>
2016-10-26 23:48:58 +0530

    setimeout accepting a callback added

--- rev_objects_and_functions/49_closures_and_callbacks/index.js
+++ rev_objects_and_functions/49_closures_and_callbacks/index.js
@@ -2,7 +2,11 @@ function sayHiLAter() {
 
 	var greeting = 'Hi';
 
+	setTimeout(function() {
+		console.log(greeting);
+	}, 3000);
 }
+sayHiLAter();
 
 function hello(param1, param2, callback) {
 	callback(param1, param2);
