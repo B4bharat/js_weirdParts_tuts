e52d118aa664c43481e4d4b940129fa41eaf71f8
Bharat Poptwani <b4bharat.available@gmail.com>
2016-10-27 23:43:51 +0530

    example and explanation of callbacks added

--- rev_objects_and_functions/49_closures_and_callbacks/closure2.js
+++ rev_objects_and_functions/49_closures_and_callbacks/closure2.js
@@ -5,3 +5,35 @@ And since the containing function has the callback function in its parameter as 
 
 Note that the callback function is not executed immediately. It is “called back” (hence the name) at some specified point inside the containing function’s body
 */
+
+// global variable​
+​var allUserData = [];
+​
+​// generic logStuff function that prints to console​
+​function logStuff (userData) {
+    if ( typeof userData === "string")
+    {
+        console.log(userData);
+    }
+    else if ( typeof userData === "object")
+    {
+        for (var item in userData) {
+            console.log(item + ": " + userData[item]);
+        }
+​
+    }
+​
+}
+​
+​// A function that takes two parameters, the last one a callback function​
+​function getInput (options, callback) {
+    allUserData.push (options);
+    callback (options);
+​
+}
+​
+​// When we call the getInput function, we pass logStuff as a parameter.​
+​// So logStuff will be the function that will called back (or executed) inside the getInput function​
+getInput ({name:"Rich", speciality:"JavaScript"}, logStuff);
+​//  name: Rich​
+​// speciality: JavaScript
