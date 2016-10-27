ebd0e0e2eaa6b65c5d5079fe08b7db51e28d518a
Bharat Poptwani <b4bharat.available@gmail.com>
2016-10-27 23:29:56 +0530

    example and explanation of callbacks added

--- rev_objects_and_functions/49_closures_and_callbacks/closure2.js
+++ rev_objects_and_functions/49_closures_and_callbacks/closure2.js
@@ -1,13 +1,3 @@
-function sayHiLAter() {
-
-	var greeting = 'Hi';
-
-	setTimeout(function() {
-		console.log(greeting);
-	}, 3000);
-}
-sayHiLAter();
-
 /*
 We can pass functions around like variables and return them in functions and use them in other functions. When we pass a callback function as an argument to another function, we are only passing the function definition. We are not executing the function in the parameter. In other words, we aren’t passing the function with the trailing pair of executing parenthesis () like we do when we are executing a function.
 
@@ -15,13 +5,3 @@ And since the containing function has the callback function in its parameter as 
 
 Note that the callback function is not executed immediately. It is “called back” (hence the name) at some specified point inside the containing function’s body
 */
-
-function hello(param1, param2, callback) {
-	//play around with parameters
-	var multiplyResult = param1*param2;
-	var divisionResult = param1/param2;
-	var average = param1+param2/2;
-
-	callback(param1, param2);
-}
-hello(3, 4, function(a, b) { console.log(a+b); });
